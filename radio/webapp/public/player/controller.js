1f1de86123f62e09c9256d44da39b212363bf94f
Adam Coulon <adamgcoulon@gmail.com>
2016-04-25 10:28:51 -0500

    development work. Youtube apis are annoying..

--- radio/webapp/public/player/controller.js
+++ radio/webapp/public/player/controller.js
@@ -1,13 +1,69 @@
 angular.module('CoffeeHouseRadio.player.controller', [])
 .controller('PlayerCtrl', ['$scope', 'MediaSocket', function($scope, MediaSocket){
-	$scope.currentSong = {
-		source: 'youtube',
-		url: 'https://www.youtube.com/watch?v=lP0D5u3EMfA',
-		time: 68
+	
+	$scope.songList = [
+		{
+			type: 'youtube',
+			id: 'sy1dYFGkPUE'
+		},
+		{
+			type: 'youtube',
+			id: 'djV11Xbc914'
+		}
+	];
+
+	$scope.currentSong = $scope.songList[0];
+
+	$scope.youtubePlayer = false;
+	var loadYoutubePlayer = function(){
+		$scope.youtubePlayer = new YT.Player('youtube-player', {
+			height: '390',
+			width: '640',
+			videoId: $scope.currentSong.id,
+			events: {
+				'onReady': playNext,
+				'onStateChange': youtubeStateChange
+			}
+		});
+	};
+
+	var startNextYoutube = function(){
+		$scope.youtubePlayer.loadVideoById({
+			'videoId': $scope.currentSong.id,
+            'suggestedQuality': 'large'
+		});
+	};
+
+	var playNext = function(){
+		if($scope.currentSong.type === 'youtube'){
+			if(!$scope.youtubePlayer){
+				loadYoutubePlayer();
+			}
+			else {
+				startNextYoutube();
+			}
+		}
+	};
+
+	var progressSong = function(){
+		$scope.currentSong = $scope.songList[Math.floor(Math.random() * $scope.songList.length)];
+	};
+
+	var youtubeStateChange = function(event){
+		console.log(event.data);
+		if(event.data != YT.PlayerState.PLAYING){
+			$scope.youtubePlayer.stopVideo();
+			progressSong();
+			playNext();
+        }
 	};
 
 	// cleanup listeners
 	$scope.$on('$destroy', function(event){
 		MediaSocket.removeAllListeners();
 	});
+
+	// start radio
+	playNext();
+
 }]);
