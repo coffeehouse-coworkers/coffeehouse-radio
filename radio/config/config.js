6d1fb716e5adafd24fb93067fa0d20c28f82f9ca
Adam Coulon <adamgcoulon@gmail.com>
2016-05-02 08:42:15 -0500

    updated redirect url

--- radio/config/config.js
+++ radio/config/config.js
@@ -21,6 +21,6 @@ module.exports = {
 	soundcloud: {
 		clientId: process.env.SC_CLIENT_ID || 'CLIENT_ID',
 		clientSecret: process.env.SC_CLIENT_SECRET || 'CLIENT_SECRET',
-		redirectUri: "www.coffeehousecoworkers.com/redirect"
+		redirectUri: "http://www.coffeehousecoworkers.com/redirect"
 	}
 }
