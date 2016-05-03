7ed5db13e88f66d3c0bf1e26b221b4a8a4365d71
Adam Coulon <adamgcoulon@gmail.com>
2016-05-02 23:21:22 -0500

    syncing using trackId instead of playlist index

--- radio/api/controllers.js
+++ radio/api/controllers.js
@@ -29,14 +29,14 @@ exports.getLiveInfo = function(request, reply){
 	let elapsedTime = overallDifference % playlistDuration; // handles loop around
 
 	let seekTime = 0;
-	let trackIndex = 0;
+	let trackId = null;
 
 	for(let i = 0; i < radioState.playlist.tracks.length; i++){
 
 		let trackDuration = radioState.playlist.tracks[i].duration;
 		if(trackDuration > elapsedTime){
 			seekTime = elapsedTime;
-			trackIndex = i;
+			trackId = radioState.playlist.tracks[i].id;
 			break;
 		}
 		elapsedTime = elapsedTime - trackDuration;
@@ -44,9 +44,74 @@ exports.getLiveInfo = function(request, reply){
 
 	let response = {
 		playlistUrl: radioState.playlist.permalink_url,
-		trackIndex: trackIndex,
+		trackId: trackId,
 		seekTime: seekTime
 	};
 
 	reply(response);
 };
+
+
+// For reference:
+
+// Initializing with playlist { duration: 73423002,
+//   release_day: null,
+//   permalink_url: 'http://soundcloud.com/tifffanaynay/sets/gro0ovy',
+//   genre: '',
+//   permalink: 'gro0ovy',
+//   purchase_url: null,
+//   release_month: null,
+//   description: '',
+//   uri: 'https://api.soundcloud.com/playlists/47565276',
+//   label_name: '',
+//   tag_list: '',
+//   release_year: null,
+//   track_count: 181,
+//   user_id: 105978028,
+//   last_modified: '2014/08/21 03:23:01 +0000',
+//   license: 'all-rights-reserved',
+//   tracks: 
+//    [ { kind: 'track',
+//        id: 66993061,
+//        created_at: '2012/11/11 15:07:36 +0000',
+//        user_id: 20910964,
+//        duration: 379410,
+//        commentable: true,
+//        state: 'finished',
+//        original_content_size: 15384428,
+//        last_modified: '2016/03/18 10:14:14 +0000',
+//        sharing: 'public',
+//        tag_list: '',
+//        permalink: 'netsky-eyes-closed',
+//        streamable: true,
+//        embeddable_by: 'all',
+//        downloadable: false,
+//        purchase_url: null,
+//        label_id: null,
+//        purchase_title: null,
+//        genre: '',
+//        title: 'Netsky - Eyes Closed',
+//        description: '',
+//        label_name: '',
+//        release: '',
+//        track_type: null,
+//        key_signature: null,
+//        isrc: null,
+//        video_url: null,
+//        bpm: null,
+//        release_year: null,
+//        release_month: null,
+//        release_day: null,
+//        original_format: 'mp3',
+//        license: 'all-rights-reserved',
+//        uri: 'https://api.soundcloud.com/tracks/66993061',
+//        user: [Object],
+//        permalink_url: 'http://soundcloud.com/jefersonsalles/netsky-eyes-closed',
+//        artwork_url: null,
+//        waveform_url: 'https://w1.sndcdn.com/r1lqqw3wDJtg_m.png',
+//        stream_url: 'https://api.soundcloud.com/tracks/66993061/stream',
+//        playback_count: 158002,
+//        download_count: 0,
+//        favoritings_count: 3214,
+//        comment_count: 36,
+//        attachments_uri: 'https://api.soundcloud.com/tracks/66993061/attachments' },
--- radio/webapp/public/player/controller.js
+++ radio/webapp/public/player/controller.js
@@ -4,13 +4,32 @@ angular.module('CoffeeHouseRadio.player.controller', [])
 	$scope.scWidget = null;
 
 	var activePlaylist = null;
-	var activeTrack = null;
+	var activeTrackId = null;
 	var activeTime = null;
 
-	function songIsPlaying(){
+	function lookupPlaylistIndex(trackList, trackId){
+		let playlistIndex = null;
+		for(let i = 0; i < trackList.length; i++){
+			if(trackList[i].id === trackId){
+				playlistIndex = i;
+				break;
+			}
+		}
+		return playlistIndex;
+	}
+
+	function trackIsPlaying(){
 		console.log("Jumping to live time");
 		$scope.scWidget.seekTo(activeTime);
 		$scope.scWidget.unbind(SC.Widget.Events.PLAY);
+	}
+
+	function tracksLoaded(){
+		$scope.scWidget.getSounds(function(trackList){
+			$scope.scWidget.skip(lookupPlaylistIndex(trackList, activeTrackId));
+			$scope.scWidget.play();
+		});
+		$scope.scWidget.unbind(SC.Widget.Events.READY);
 	}
 
 	function restSucessHandler(response){
@@ -19,20 +38,19 @@ angular.module('CoffeeHouseRadio.player.controller', [])
 		var liveSongInfo = response.data;
 		console.log("Live Info: ", liveSongInfo);
 		activePlaylist = liveSongInfo.playlistUrl;
-		activeTrack = liveSongInfo.trackIndex;
+		activeTrackId = liveSongInfo.trackId;
 		activeTime = liveSongInfo.seekTime;
 
 		// load widget
 		$scope.scWidget = SC.Widget("sc-widget");
 
 		// bind event listeners
-		$scope.scWidget.bind(SC.Widget.Events.PLAY, songIsPlaying);	
+		$scope.scWidget.bind(SC.Widget.Events.PLAY, trackIsPlaying);	
+		$scope.scWidget.bind(SC.Widget.Events.READY, tracksLoaded);
 
 		// load playlist
 		$scope.scWidget.load(activePlaylist, {
-			auto_play: true,
-			show_comments: false,
-			start_track: activeTrack
+			show_comments: false
 		});
 	}
 
@@ -46,3 +64,37 @@ angular.module('CoffeeHouseRadio.player.controller', [])
 
 	initialize();
 }]);
+
+
+// For reference:
+
+// _resource_id: 149294836
+// _resource_type: "sound"
+// artwork_url: "https://i1.sndcdn.com/artworks-000079294323-l701zd-large.jpg"
+// comment_count: 52
+// commentable: true
+// created_at: "2014-05-13T09:35:58Z"
+// description: "'The Whistle Song' taken from Netsky's album '2'.
+// ↵Get it here http://smarturl.it/netsky2 and here http://hospi.tl/netsky2"
+// domain_lockings: Array[0]
+// download_count: 0
+// download_url: null
+// downloadable: false
+// duration: 191912
+// embeddable_by: "all"
+// full_duration: 191912
+// genre: ""
+// has_downloads_left: true
+// id: 149294836
+// kind: "track"
+// label_name: ""
+// last_modified: "2016-04-05T12:06:26Z"
+// license: "all-rights-reserved"
+// likes_count: 3031
+// monetization_model: "NOT_APPLICABLE"
+// permalink: "the-whistle-song-3"
+// permalink_url: "https://soundcloud.com/netsky/the-whistle-song-3"
+// playable: true
+// playback_count: 215564
+// policy: "ALLOW"
+// public: true
